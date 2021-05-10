1b6c31e54278555f91db989428e6d213efd7b625
ahmedali8 <imahmedalibhatti@gmail.com>
2021-05-11 00:58:34 +0500

    demonstration of console.log statements in hardhat ethersjs

--- contracts/CarbonToken.sol
+++ contracts/CarbonToken.sol
@@ -4,13 +4,15 @@ pragma solidity ^0.8.0;
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.1/contracts/token/ERC20/ERC20.sol";
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 contract CarbonToken is ERC20 {
-    constructor() ERC20("CARBON", "CRB")  {
-        _mint(_msgSender(), 300000000 * (10 ** uint256(decimals())));
+    constructor() ERC20("CARBON", "CRB") {
+        _mint(_msgSender(), 300000000 * (10**uint256(decimals())));
     }
 
     function faucet(address _account, uint256 _amount) public {
+        console.log("send %s tokens to %s", _amount, _account);
         _mint(_account, _amount);
     }
 }
--- test/CarbonToken.test.js
+++ test/CarbonToken.test.js
@@ -1,31 +1,36 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 
-const { toWei } = require('./utils.js');
+const { toWei } = require("./utils.js");
 
-describe('CarbonToken', () => {
+describe("CarbonToken", () => {
   let carbonToken;
 
   beforeEach(async () => {
     [owner, user, ...accounts] = await ethers.getSigners();
 
-    const CarbonToken = await ethers.getContractFactory('CarbonToken');
+    const CarbonToken = await ethers.getContractFactory("CarbonToken");
     carbonToken = await CarbonToken.deploy();
   });
 
-  describe('CarbonToken Deployment', async () => {
-    it('has a name', async () => {
+  describe("CarbonToken Deployment", async () => {
+    it("has a name", async () => {
       const name = await carbonToken.name();
-      expect(name).to.equal('CARBON');
+      expect(name).to.equal("CARBON");
     });
 
-    it('has a symbol', async () => {
+    it("has a symbol", async () => {
       const symbol = await carbonToken.symbol();
-      expect(symbol).to.equal('CRB');
+      expect(symbol).to.equal("CRB");
     });
 
-    it('checks if owner has the tokens', async () => {
+    it("checks if owner has the tokens", async () => {
       const balance = await carbonToken.balanceOf(owner.address);
-      expect(balance).to.equal(toWei('300000000'));
+      expect(balance).to.equal(toWei("300000000"));
+    });
+
+    it("sends 100 tokens to user", async () => {
+      await carbonToken.connect(owner).faucet(user.address, toWei("100"));
+      expect(await carbonToken.balanceOf(user.address)).to.equal(toWei("100"));
     });
   });
 });
