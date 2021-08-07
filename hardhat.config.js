a5fbb3b7824348033fd86caf9aadfcc6db04c2e1
ahmedali <imahmedalibhatti@gmail.com>
2021-08-07 13:31:28 +0500

    refactor: - create a function for getting config a/c to netowrk - separate tasks and require them here

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,14 +16,42 @@ require("solidity-coverage");
 require("hardhat-contract-sizer");
 // hardhat gas reporter (uncomment to enable)
 require("hardhat-gas-reporter");
+// A plugin that brings OVM compiler support to Hardhat projects.
+require("@eth-optimism/plugins/hardhat/compiler");
 
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+// require tasks
+require("./tasks/accounts");
+require("./tasks/flatfile");
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const infuraUrl = (network) =>
+  `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+
+const networks = {
+  arbitrummainnet: { chainId: 42161, url: infuraUrl("arbitrum-mainnet") },
+  arbitrumrinkeby: { chainId: 421611, url: infuraUrl("arbitrum-rinkeby") },
+  bscmainnet: { chainId: 56, url: process.env.BSC_MAINNET_RPC_URL },
+  bsctestnet: { chainId: 97, url: process.env.BSC_MAINNET_RPC_URL },
+  ganache: { chainId: 1337, url: "http://127.0.0.1:7545" },
+  goerli: { chainId: 5, url: infuraUrl("goerli") },
+  hardhat: { chainId: 31337 },
+  kovan: { chainId: 42, url: infuraUrl("kovan") },
+  mainnet: { chainId: 1, url: infuraUrl("mainnet") },
+  maticmainnet: { chainId: 137, url: infuraUrl("polygon-mainnet") },
+  maticmumbai: {
+    chainId: 80001,
+    url: infuraUrl("polygon-mumbai"),
+  },
+  optimismmainnet: {
+    chainId: 1,
+    url: infuraUrl("optimism-mainnet"),
+  },
+  optimismkovan: {
+    chainId: 1,
+    url: infuraUrl("optimism-kovan"),
+  },
+  rinkeby: { chainId: 4, url: infuraUrl("rinkeby") },
+  ropsten: { chainId: 3, url: infuraUrl("ropsten") },
+};
 
 // can add as many private keys as you want
 const accounts = [
@@ -33,6 +61,15 @@ const accounts = [
   // `0x${process.env.PRIVATE_KEY_4}`,
   // `0x${process.env.PRIVATE_KEY_5}`,
 ];
+
+function getChainConfig(network) {
+  return {
+    accounts,
+    chainId: networks[network].chainId,
+    from: accounts[0],
+    url: networks[network].url,
+  };
+}
 
 const contractSizer = () =>
   process.env.CONTRACT_SIZER
@@ -44,6 +81,69 @@ const contractSizer = () =>
     : null;
 
 module.exports = {
+  abiExporter: {
+    path: "./abi_exporter",
+    clear: true,
+    flat: true,
+    // only: [':ERC20$'],
+    spacing: 2,
+  },
+  contractSizer,
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey:
+      process.env.ETHERSCAN_API_KEY || process.env.BSCSCAN_API_KEY || null,
+  },
+  gasReporter: {
+    enabled: true, // set false to disable
+    // enabled: process.env.REPORT_GAS ? true : false, // this is not working as of now
+    currency: "USD",
+    // if commented out then it fetches from ethGasStationAPI
+    // gasPrice: process.env.GAS_PRICE,
+    coinmarketcap: process.env.COIN_MARKET_CAP_API_KEY || null,
+    excludeContracts: [],
+    src: "./contracts",
+  },
+  networks: {
+    hardhat: {
+      chainId: networks["hardhat"].chainId,
+    },
+    ganache: {
+      chainId: networks["ganache"].chainId,
+      url: networks["ganache"].url,
+    },
+
+    /* ETHEREUM */
+    mainnet: getChainConfig("mainnet"),
+    ropsten: getChainConfig("ropsten"),
+    rinkeby: getChainConfig("rinkeby"),
+    goerli: getChainConfig("goerli"),
+
+    /* BINANCE SMART CHAIN */
+    bscmainnet: getChainConfig("bscmainnet"),
+    bsctestnet: getChainConfig("bsctestnet"),
+
+    /* MATIC L2 */
+    maticmainnet: getChainConfig("maticmainnet"),
+    maticmumbai: getChainConfig("maticmumbai"),
+
+    /* ARBITRUM L2 */
+    arbitrummainnet: getChainConfig("arbitrummainnet"),
+    arbitrumrinkeby: getChainConfig("arbitrumrinkeby"),
+
+    /* OPTIMISM L2 */
+    optimismmainnet: getChainConfig("optimismmainnet"),
+    optimismkovan: getChainConfig("optimismkovan"),
+  },
+  ovm: {
+    solcVersion: "0.8.4", // Your version goes here.
+  },
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
   solidity: {
     compilers: [
       {
@@ -58,86 +158,5 @@ module.exports = {
         },
       },
     ],
-  },
-  networks: {
-    // hardhat: {
-    //   chainId: 1337,
-    // },
-    localhost: {
-      url: "http://127.0.0.1:7545",
-    },
-    /* ETHEREUM TESTNETS */
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts,
-      from: `0x${process.env.PRIVATE_KEY_1}`,
-      chainId: 3,
-    },
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts,
-      from: `0x${process.env.PRIVATE_KEY_1}`,
-      chainId: 4,
-    },
-    goerli: {
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts,
-      from: `0x${process.env.PRIVATE_KEY_1}`,
-      chainId: 5,
-    },
-    /* BINANCE SMART CHAIN */
-    bscmainnet: {
-      url: process.env.BSC_MAINNET_RPC_URL,
-      accounts,
-      from: `0x${process.env.PRIVATE_KEY_1}`,
-      chainId: 56,
-    },
-    bsctestnet: {
-      url: process.env.BSC_TESTNET_RPC_URL,
-      accounts,
-      from: `0x${process.env.PRIVATE_KEY_1}`,
-      chainId: 97,
-    },
-    /* MATIC L2 */
-    maticmainnet: {
-      url: process.env.MATIC_MAINNET_RPC_URL,
-      accounts,
-      from: `0x${process.env.PRIVATE_KEY_1}`,
-      chainId: 137,
-    },
-    matictestnet: {
-      url: process.env.MATIC_MUMBAI_TESTNET_RPC_URL,
-      accounts,
-      from: `0x${process.env.PRIVATE_KEY_1}`,
-      chainId: 80001,
-    },
-  },
-  etherscan: {
-    apiKey:
-      process.env.ETHERSCAN_API_KEY || process.env.BSCSCAN_API_KEY || null,
-  },
-  abiExporter: {
-    path: "./abi_exporter",
-    clear: true,
-    flat: true,
-    // only: [':ERC20$'],
-    spacing: 2,
-  },
-  paths: {
-    sources: "./contracts",
-    tests: "./test",
-    cache: "./cache",
-    artifacts: "./artifacts",
-  },
-  contractSizer,
-  gasReporter: {
-    enabled: true, // set false to disable
-    // enabled: process.env.REPORT_GAS ? true : false, // this is not working as of now
-    currency: "USD",
-    // if commented out then it fetches from ethGasStationAPI
-    // gasPrice: process.env.GAS_PRICE,
-    coinmarketcap: process.env.COIN_MARKET_CAP_API_KEY || null,
-    excludeContracts: [],
-    src: "./contracts",
   },
 };
