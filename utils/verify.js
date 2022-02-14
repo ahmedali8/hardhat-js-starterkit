a7128c576216aa78b3d1867ac8ac4bc96ce49acd
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-02-14 19:56:58 +0500

    refactor: optional tx and document function

--- utils/verify.js
+++ utils/verify.js
@@ -2,19 +2,28 @@ const { ethers, run } = require("hardhat");
 const { delayLog } = require("./index");
 
 async function waitForConfirmations(tx, confirmations = 5) {
+  if (!tx) return;
   const { hash } = tx;
   console.log(`waiting for ${confirmations} confirmations ...`);
   await ethers.provider.waitForTransaction(hash, confirmations);
 }
 
+/**
+ * Programmatically verify a contract
+ * @param {*} contractAddress contract address in string
+ * @param {*} args constructor args in array
+ * @param tx optional tx
+ * @param options optionally set {ms, doConfirmation, confirm}
+ */
 async function verifyContract(
   contractAddress,
-  tx,
   args = [],
+  tx,
   options = { ms: 60_000, doConfirmation: true, confirm: 5 }
 ) {
   const { ms, doConfirmation, confirm } = options;
-  doConfirmation && waitForConfirmations(tx, confirm);
+  // wait for confirmation if tx is available
+  if (doConfirmation && tx) waitForConfirmations(tx, confirm);
   await delayLog(ms);
 
   await run("verify:verify", {
