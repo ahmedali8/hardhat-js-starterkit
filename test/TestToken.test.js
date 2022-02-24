32f1816328105c0742701e2284749e2a3e6634c6
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-02-24 19:43:15 +0500

    test: add failing test example

--- test/TestToken.test.js
+++ test/TestToken.test.js
@@ -1,8 +1,9 @@
 const { expect } = require("chai");
 const { toWei } = require("../utils/index.js");
 const { deployContract } = require("../utils/contracts");
+const { ZERO_ADDRESS } = require("../utils/constants");
 
-describe("TestingContract", () => {
+describe("TestingContract Unit tests", () => {
   let token;
 
   beforeEach(async () => {
@@ -11,7 +12,7 @@ describe("TestingContract", () => {
     token = await deployContract(owner, "TestingContract", args);
   });
 
-  describe("TestingContract Deployment", async () => {
+  describe("#constructor", async () => {
     it("has a name", async () => {
       const name = await token.name();
       expect(name).to.equal("TEST_TOKEN");
@@ -26,10 +27,18 @@ describe("TestingContract", () => {
       const balance = await token.balanceOf(owner.address);
       expect(balance).to.equal(toWei("1000000"));
     });
+  });
 
-    it("sends 100 tokens to user", async () => {
+  describe("#mint", () => {
+    it("mints 100 tokens to user", async () => {
       await token.connect(owner).mint(user.address, toWei("100"));
       expect(await token.balanceOf(user.address)).to.equal(toWei("100"));
+    });
+
+    it("fails to mint if account is zero address", async () => {
+      await expect(
+        token.connect(accounts[0]).mint(ZERO_ADDRESS, toWei("1"))
+      ).to.be.revertedWith("ERC20: mint to the zero address");
     });
   });
 });
