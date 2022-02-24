a1f347e258a70c2a0fba7c16d621ad2a678b5d27
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-02-24 20:36:53 +0500

    refactor: update network names and add api key to specific networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,6 @@
 require("@nomiclabs/hardhat-waffle");
 // verify contract on etherscan
 require("@nomiclabs/hardhat-etherscan");
-// for exporting abi in separate file
-require("hardhat-abi-exporter");
 // sol coverage
 require("solidity-coverage");
 // hardhat contract sizer
@@ -32,8 +30,6 @@ if (
 }
 
 const INFURA_KEY = process.env.INFURA_PROJECT_ID;
-const BSC_MAINNET_KEY = process.env.BSC_MAINNET_RPC_URL;
-const BSC_TESTNET_KEY = process.env.BSC_TESTNET_RPC_URL;
 if (typeof INFURA_KEY === "undefined") {
   throw new Error(`INFURA_PROJECT_ID must be a defined environment variable`);
 }
@@ -41,39 +37,83 @@ if (typeof INFURA_KEY === "undefined") {
 const infuraUrl = (network) => `https://${network}.infura.io/v3/${INFURA_KEY}`;
 
 const networks = {
-  arbone: {
-    chainId: 42161,
-    url: infuraUrl("arbitrum-mainnet"),
-  },
-  arbrinkeby: {
-    chainId: 421611,
-    url: infuraUrl("arbitrum-rinkeby"),
-  },
-  bnb: { chainId: 56, url: BSC_MAINNET_KEY },
-  bnbt: { chainId: 97, url: BSC_TESTNET_KEY },
-  ganache: { chainId: 1337, url: "http://127.0.0.1:7545" },
-  goerli: { chainId: 5, url: infuraUrl("goerli") },
   hardhat: { chainId: 31337 },
-  kovan: { chainId: 42, url: infuraUrl("kovan") },
-  mainnet: { chainId: 1, url: infuraUrl("mainnet") },
-  matic: {
+  ganache: { chainId: 1337, url: "http://127.0.0.1:7545" },
+
+  // ETHEREUM
+  mainnet: {
+    chainId: 1,
+    url: infuraUrl("mainnet"),
+    apiKey: process.env.ETHERSCAN_API_KEY || null,
+  },
+  kovan: {
+    chainId: 42,
+    url: infuraUrl("kovan"),
+    apiKey: process.env.ETHERSCAN_API_KEY || null,
+  },
+  goerli: {
+    chainId: 5,
+    url: infuraUrl("goerli"),
+    apiKey: process.env.ETHERSCAN_API_KEY || null,
+  },
+  rinkeby: {
+    chainId: 4,
+    url: infuraUrl("rinkeby"),
+    apiKey: process.env.ETHERSCAN_API_KEY || null,
+  },
+  ropsten: {
+    chainId: 3,
+    url: infuraUrl("ropsten"),
+    apiKey: process.env.ETHERSCAN_API_KEY || null,
+  },
+
+  // BINANCE SMART CHAIN
+  bsc: {
+    chainId: 56,
+    url: process.env.BSC_MAINNET_RPC_URL,
+    apiKey: process.env.BSCSCAN_API_KEY || null,
+  },
+  bscTestnet: {
+    chainId: 97,
+    url: process.env.BSC_TESTNET_RPC_URL,
+    apiKey: process.env.BSCSCAN_API_KEY || null,
+  },
+
+  // MATIC/POLYGON
+  polygon: {
     chainId: 137,
     url: infuraUrl("polygon-mainnet"),
+    apiKey: process.env.POLYGONSCAN_API_KEY || null,
   },
-  maticmum: {
+  polygonMumbai: {
     chainId: 80001,
     url: infuraUrl("polygon-mumbai"),
+    apiKey: process.env.POLYGONSCAN_API_KEY || null,
   },
-  optimism: {
+
+  // OPTIMISM
+  optimisticEthereum: {
     chainId: 10,
     url: infuraUrl("optimism-mainnet"),
+    apiKey: process.env.OPTIMISTIC_ETHERSCAN_API_KEY || null,
   },
-  optimismkovan: {
+  optimisticKovan: {
     chainId: 69,
     url: infuraUrl("optimism-kovan"),
+    apiKey: process.env.OPTIMISTIC_ETHERSCAN_API_KEY || null,
   },
-  rinkeby: { chainId: 4, url: infuraUrl("rinkeby") },
-  ropsten: { chainId: 3, url: infuraUrl("ropsten") },
+
+  // ARBITRUM
+  arbitrumOne: {
+    chainId: 42161,
+    url: infuraUrl("arbitrum-mainnet"),
+    apiKey: process.env.ARBISCAN_API_KEY || null,
+  },
+  arbitrumTestnet: {
+    chainId: 421611,
+    url: infuraUrl("arbitrum-rinkeby"),
+    apiKey: process.env.ARBISCAN_API_KEY || null,
+  },
 };
 
 // can add as many private keys as you want
@@ -97,25 +137,33 @@ function getChainConfig(network) {
     accounts,
     chainId: networks[network].chainId,
     url: networks[network].url,
+    apiKey: networks[network].apiKey,
   };
 }
 
 module.exports = {
-  abiExporter: {
-    path: "./abi_exporter",
-    clear: true,
-    flat: true,
-    spacing: 2,
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: process.env.CONTRACT_SIZER ? true : false,
+    disambiguatePaths: false,
   },
-  contractSizer: () =>
-    process.env.CONTRACT_SIZER && {
-      alphaSort: true,
-      runOnCompile: true,
-      disambiguatePaths: false,
-    },
   defaultNetwork: "hardhat",
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || null,
+    apiKey: {
+      // ETHEREUM
+      mainnet: getChainConfig("mainnet").apiKey,
+      ropsten: getChainConfig("ropsten").apiKey,
+      rinkeby: getChainConfig("rinkeby").apiKey,
+      goerli: getChainConfig("goerli").apiKey,
+
+      // BINANCE SMART CHAIN
+      bsc: getChainConfig("bsc").apiKey,
+      bscTestnet: getChainConfig("bscTestnet").apiKey,
+
+      // MATIC/POLYGON
+      polygon: getChainConfig("polygon").apiKey,
+      polygonMumbai: getChainConfig("polygonMumbai").apiKey,
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS ? true : false,
@@ -135,27 +183,19 @@ module.exports = {
       url: networks["ganache"].url,
     },
 
-    /* ETHEREUM */
+    // ETHEREUM
     mainnet: getChainConfig("mainnet"),
     ropsten: getChainConfig("ropsten"),
     rinkeby: getChainConfig("rinkeby"),
     goerli: getChainConfig("goerli"),
 
-    /* BINANCE SMART CHAIN */
-    bnb: getChainConfig("bnb"),
-    bnbt: getChainConfig("bnbt"),
+    // BINANCE SMART CHAIN
+    bsc: getChainConfig("bsc"),
+    bscTestnet: getChainConfig("bscTestnet"),
 
-    /* MATIC L2 */
-    matic: getChainConfig("matic"),
-    maticmum: getChainConfig("maticmum"),
-
-    /* ARBITRUM L2 */
-    arb: getChainConfig("arbone"),
-    arbrinkeby: getChainConfig("arbrinkeby"),
-
-    /* OPTIMISM L2 */
-    optimism: getChainConfig("optimism"),
-    optimismkovan: getChainConfig("optimismkovan"),
+    // MATIC/POLYGON
+    polygon: getChainConfig("polygon"),
+    polygonMumbai: getChainConfig("polygonMumbai"),
   },
   paths: {
     artifacts: "./artifacts",
