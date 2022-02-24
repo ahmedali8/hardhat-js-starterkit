a7b458cda21bd50c46f4b6064c4a1ec5bf6774a7
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-02-24 19:41:19 +0500

    chore: bump sol version to 0.8.12

--- hardhat.config.js
+++ hardhat.config.js
@@ -166,7 +166,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.11",
+        version: "0.8.12",
         settings: {
           metadata: {
             // Not including the metadata hash
