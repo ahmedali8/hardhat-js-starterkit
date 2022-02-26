53a6b6ec8147cd73d27f89f10c14dd2c60dcfe94
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-02-26 18:30:07 +0500

    chore: esint config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  ignorePatterns: ["node_modules/**/*"],
+  extends: ["eslint:recommended", "prettier"],
+  rules: {
+    "no-unused-vars": "warn",
+    "no-extra-boolean-cast": "off",
+    "no-constant-condition": "off",
+    "no-async-promise-executor": "off",
+    "prefer-destructuring": "off",
+    "prefer-template": "off",
+    "no-console": "off",
+    "func-names": "off",
+  },
+};
