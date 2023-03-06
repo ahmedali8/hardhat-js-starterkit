a89b993049ecb6dedfef9639d27a69466c9779a6
Ahmed Ali <imahmedalibhatti@gmail.com>
2023-03-06 12:55:23 +0500

    feat: update sol version to v0.8.19

--- hardhat.config.js
+++ hardhat.config.js
@@ -114,7 +114,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
+        version: "0.8.19",
         settings: {
           metadata: {
             // Not including the metadata hash
